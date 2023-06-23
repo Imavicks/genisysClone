21d360add53226151fe2fc272f72828ff4b8181c
Imavicks <victor.imazobi@gmail.com>
2023-06-23 16:48:32 +0100

    update cart page

--- resources/js/cart.js
+++ resources/js/cart.js
@@ -9,40 +9,32 @@ const cartItemSize = document.querySelector('.cart-item-size');
 const cartItemWidth = document.querySelector('.cart-item-width');
 
 cartItemImage.src = product.image;
-cartItemPrice.innerHTML = `<b>&#8358;<b>${product.price}`;
 cartItemColor.innerHTML = `Color: ${product.color}`;
 cartItemSize.innerHTML = `Select Size: ${product.size}`;
 cartItemWidth.innerHTML = `Select Width: ${product.width}`;
-
-// Add event listener to remove items button
-const removeItemsButton = document.getElementById('remove-items');
-removeItemsButton.addEventListener('click', () => {
-  // Clear the session storage
-  sessionStorage.clear();
-  // Remove the product details from the cart page
-  cartItemImage.src = '';
-  cartItemPrice.innerHTML = 'Price: &#8358;0';
-  cartItemColor.innerHTML = 'Color: ';
-  cartItemSize.innerHTML = 'Size: ';
-  cartItemWidth.innerHTML = 'Width: ';
-});
 
 // Increment and Decrement Buttons
 const decrementCount = document.getElementById('decrease-count');
 const incrementCount = document.getElementById('increase-count');
 const totalCount = document.getElementById('total-count');
+const cartCountIndicator = document.getElementById('cart-count'); // Cart count indicator element
 
-// Variable to track count
-let count = 0;
+// Variable to track count and update price
+let count = 1; // Set the initial count to 1
 
-// Display initial count value
-totalCount.innerHTML = count;
+// Function to update price display and cart count indicator
+const updatePriceAndCount = () => {
+  const totalPrice = product.price * count;
+  cartItemPrice.innerHTML = `<b>&#8358;<b>${product.price} x ${count} = &#8358;${totalPrice}`;
+  cartCountIndicator.textContent = count; // Update cart count indicator
+};
 
 // Function to decrement count
 const handleDecrement = () => {
-  if (count > 0) {
+  if (count > 1) {
     count -= 1;
     totalCount.innerHTML = count;
+    updatePriceAndCount();
   }
 };
 
@@ -50,11 +42,44 @@ const handleDecrement = () => {
 const handleIncrement = () => {
   count += 1;
   totalCount.innerHTML = count;
+  updatePriceAndCount();
 };
+
+// Display initial count value and update price display
+totalCount.innerHTML = count;
+updatePriceAndCount();
 
 // Add click event to buttons
 decrementCount.addEventListener('click', handleDecrement);
 incrementCount.addEventListener('click', handleIncrement);
+
+// Add event listener to remove items button
+const removeItemsButton = document.getElementById('remove-items');
+const section1 = document.querySelector('.section-1');
+const section2 = document.querySelector('.section-2');
+const backButton = document.createElement('button');
+
+removeItemsButton.addEventListener('click', () => {
+  // Clear the session storage
+  sessionStorage.clear();
+  // Hide the section
+  section1.classList.add('hidden');
+  // Reset count and update cart count indicator
+  count = 0;
+  totalCount.innerHTML = count;
+  updatePriceAndCount();
+  cartCountIndicator.textContent = count;
+  // Apply conditional styling to section-2
+  section2.classList.add('section-2-adjusted');
+  // Add back arrow button
+  backButton.innerHTML = '< Back';
+  backButton.classList.add('back-button');
+  backButton.addEventListener('click', () => {
+    // Go back to the product page
+    window.history.back();
+  });
+  section2.prepend(backButton);
+});
 
 // Add event listener to checkout button
 const checkoutButton = document.querySelector('.checkout-button button');
