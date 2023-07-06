eb86d62d33af1b55928e7ceb9be0b0e29617b17c
Kwenyerechukwu Paul <paulkwenye@gmail.com>
2023-07-06 21:36:25 +0300

    fix mobile response

--- resources/js/checkout.js
+++ resources/js/checkout.js
@@ -61,3 +61,59 @@ const finalCheckoutPrice = subtotal + shippingCost + tax;
 
 // Set the calculated value to the checkout total element
 checkoutTotal.innerHTML = finalCheckoutPrice.toLocaleString();
+
+// RESIZE MOBILE VIEW FOR HEADER
+
+const mobileHeader = `<section class="title">
+        <nav class="navbar navbar-expand-lg bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand fw-bolder text-white" href="#">KICKIN' IT</a>
+            <button class="navbar-toggler bg-dark text-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon bg-light"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item bg-dark">
+                <a class="nav-link text-white" aria-current="page" href="#">About</a>
+                </li>
+                <li class="nav-item bg-dark">
+                <a class="nav-link text-white" href="#">Contact us</a>
+                </li>
+                <li class="nav-item bg-dark">
+                <a class="nav-link text-white">Our location</a>
+                </li>
+                <li class="nav-item bg-dark">
+                <a class="nav-link text-white">Return order</a>
+                </li>
+                <li class="nav-item bg-dark">
+                <a class="nav-link text-white">Delivery</a>
+                </li>
+                <li class="nav-item bg-dark">
+                <a class="nav-link text-white">Help</a>
+                </li>
+            </ul>
+            <form class="d-flex bg-dark" role="search">
+                <input class="form-control me-2 " type="search" placeholder="Search Product" aria-label="Search">
+                <button class="btn btn-outline-light" type="submit">Search</button>
+            </form>
+            </div>
+        </div>
+        </nav>
+    </section>`;
+
+    const originalHeader = document.querySelector(".main").innerHTML;
+    const headDirectory = document.querySelector(".directory");
+    function changeHeader() {
+      const desktopHeader = document.querySelector(".main");
+      
+
+      if (window.innerWidth <= 744) {
+        desktopHeader.innerHTML = mobileHeader;
+        headDirectory.style.display = "none";
+      } else{
+        desktopHeader.innerHTML = originalHeader;
+      }
+    }
+
+    changeHeader();
+    window.addEventListener('resize', changeHeader)
